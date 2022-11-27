e9d1c701afe96649283f99d1303d1ae0dae9a97b
STEVENPC\Steven <stevenfgc@hotmail.com>
2022-11-27 14:17:10 -0500

    uploaded with name

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -2,7 +2,6 @@ import { storage } from "./config";
 import { ref, uploadBytes } from "firebase/storage";
 
 //Upload an image to firebase storage
-const storageRef = ref(storage, "images");
 
 export const uploadImageToFirebase = async (file) => {
   //   const fileRef = storageRef.child(file.name);
@@ -10,7 +9,7 @@ export const uploadImageToFirebase = async (file) => {
   //     console.log("Uploaded a blob or file!");
   //   });
   //   return await fileRef.getDownloadURL();
-
+  const storageRef = ref(storage, file.name);
   uploadBytes(storageRef, file)
     .then((snapshot) => {
       console.log("Uploaded a blob or file!");
